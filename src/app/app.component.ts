53b898ac5d79437c3175f94324c1420db0b76856
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-08-23 23:47:35 +0000

    feat: updated to display stat card only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./auth/login/login.component";
+import {StatCardComponent} from "./components/stat-card/stat-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, StatCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

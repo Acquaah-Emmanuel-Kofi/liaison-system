544896b39dd8d6532d0f38d6462e4bcde47142ce
waglarry <emmanuelacquaah2468@gmail.com>
2024-08-30 12:11:36 +0000

    feat: render links on profile card

--- src/app/shared/components/navbar/components/profile-card/profile-card.component.ts
+++ src/app/shared/components/navbar/components/profile-card/profile-card.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { ILinks } from '../../../../interfaces/links.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'liaison-profile-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss'
+  styleUrl: './profile-card.component.scss',
 })
 export class ProfileCardComponent {
+  emitMenu = output<string>();
 
+  cardItems: ILinks[] = [
+    {
+      label: 'Your Profile',
+      link: '/profile',
+    },
+    {
+      label: 'Settings',
+      link: '/settings',
+    },
+  ];
+
+  onMenuClick(menu: string): void {
+    this.emitMenu.emit(menu);
+  }
 }

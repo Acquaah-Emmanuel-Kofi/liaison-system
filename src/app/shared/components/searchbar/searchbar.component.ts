0555d1f60b6a7c5a902c57d848ba6bf4a68826d7
waglarry <emmanuelacquaah2468@gmail.com>
2024-08-30 14:58:19 +0000

    feat: emit search term value

--- src/app/shared/components/searchbar/searchbar.component.ts
+++ src/app/shared/components/searchbar/searchbar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'liaison-searchbar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './searchbar.component.html',
-  styleUrl: './searchbar.component.scss'
+  styleUrl: './searchbar.component.scss',
 })
 export class SearchbarComponent {
+  searchTermValue = output<string>();
+  searchTerm: string = '';
 
+  handleSearchFilter(event: string) {
+    this.searchTermValue.emit(event);
+  }
 }

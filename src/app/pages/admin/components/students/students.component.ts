dcd0fe05af3d3aa92f9062325cda42ec06d0138a
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-02 16:22:44 +0000

    feat: integrate header in students component

--- src/app/pages/admin/components/students/students.component.ts
+++ src/app/pages/admin/components/students/students.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'liaison-students',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './students.component.html',
   styleUrl: './students.component.scss'
 })

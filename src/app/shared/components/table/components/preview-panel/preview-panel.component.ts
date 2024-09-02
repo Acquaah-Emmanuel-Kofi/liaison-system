a4222d973d75f161b32ad65cbeebc362e9487c40
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-02 10:47:48 +0000

    feat: create preview panel component

---
+++ src/app/shared/components/table/components/preview-panel/preview-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-preview-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './preview-panel.component.html',
+  styleUrl: './preview-panel.component.scss'
+})
+export class PreviewPanelComponent {
+
+}

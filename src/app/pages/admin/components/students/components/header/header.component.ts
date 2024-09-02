d4aa4f564d31a66e3a3982f4a77f1c7c4c9c40b0
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-02 16:21:11 +0000

    feat: integrate header title in header component

--- src/app/pages/admin/components/students/components/header/header.component.ts
+++ src/app/pages/admin/components/students/components/header/header.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderTitleComponent } from '../../../../../../shared/components/header-title/header-title.component';
 
 @Component({
   selector: 'liaison-header',
   standalone: true,
-  imports: [],
+  imports: [HeaderTitleComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  toggledFilterButton: boolean = false
+  toggledFilterButton: boolean = false;
 
   toggleFilterButton() {
-    this.toggledFilterButton =!this.toggledFilterButton
+    this.toggledFilterButton = !this.toggledFilterButton;
   }
 }

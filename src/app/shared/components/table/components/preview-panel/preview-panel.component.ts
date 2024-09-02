24faefafc41660a925a2db7d4930efa07500f74a
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-02 11:18:29 +0000

    feat: implement preview panel with dummy data to preview selected row data

--- src/app/shared/components/table/components/preview-panel/preview-panel.component.ts
+++ src/app/shared/components/table/components/preview-panel/preview-panel.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 
 @Component({
   selector: 'liaison-preview-panel',
   standalone: true,
   imports: [],
   templateUrl: './preview-panel.component.html',
-  styleUrl: './preview-panel.component.scss'
+  styleUrl: './preview-panel.component.scss',
 })
 export class PreviewPanelComponent {
+  closeEvent = output<void>();
 
+  onClosePanel() {
+    this.closeEvent.emit();
+  }
 }

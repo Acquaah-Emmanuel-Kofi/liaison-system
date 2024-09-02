7f6849092cfa93c6de1492c0250b8f9555c6640a
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-02 19:09:41 +0000

    feat: initialized upload page

---
+++ src/app/pages/admin/components/upload/upload.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload.component.html',
+  styleUrl: './upload.component.scss'
+})
+export class UploadComponent {
+
+}

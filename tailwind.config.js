3d60b304728585384b9d265de00221d1bd2b99f4
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-02 11:22:40 +0000

    feat: updated assests, routes and tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'Primary-Blue':'#142965',
         'Border-Blue':'#142965',
         'Bright-Blue':'#475C98',
+        'Dark-Blue':'#19337E',
         'Primary-Black':'#333131',
         'Lighter-Blue':'#D1D9E2',
         'Lighter-Gray':'#F9FBFD'

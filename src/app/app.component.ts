4fc27ca8040e170ac68217f70c162be261353d04
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-02 11:22:59 +0000

    feat: updated app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./auth/login/login.component";
 import {StatCardComponent} from "./components/stat-card/stat-card.component";
+import {SidebarComponent} from "./shared/components/sidebar/sidebar.component";
+import {INavLinks} from "./shared/components/sidebar/sidebar.interface";
+import {NavbarComponent} from "./shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, StatCardComponent],
+  imports: [RouterOutlet, LoginComponent, StatCardComponent, SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'liaison-system';
+  plkisns!: INavLinks[];
 }

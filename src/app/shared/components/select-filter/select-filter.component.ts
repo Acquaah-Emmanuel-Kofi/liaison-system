ef256589410dd8269706b0c2750742313d78d204
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-03 10:10:59 +0000

    feat: create select filter component

---
+++ src/app/shared/components/select-filter/select-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-select-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-filter.component.html',
+  styleUrl: './select-filter.component.scss'
+})
+export class SelectFilterComponent {
+
+}

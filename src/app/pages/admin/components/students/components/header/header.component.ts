29be213c2f4ab2c9053f0abab6eaf5ba80919f80
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-03 12:03:30 +0000

    feat: integrate select filter in header component

--- src/app/pages/admin/components/students/components/header/header.component.ts
+++ src/app/pages/admin/components/students/components/header/header.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 import { HeaderTitleComponent } from '../../../../../../shared/components/header-title/header-title.component';
+import { SelectFilterComponent } from '../../../../../../shared/components/select-filter/select-filter.component';
 
 @Component({
   selector: 'liaison-header',
   standalone: true,
-  imports: [HeaderTitleComponent],
+  imports: [HeaderTitleComponent, SelectFilterComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   toggledFilterButton: boolean = false;
 
+  filterOptions: string[] = [
+    'Computer Science',
+    'Engineering',
+    'Fashion Design',
+    'Textiles Design',
+    'Ceramic Design',
+    'Painting Design',
+    'Sculpture Design',
+    'Graphic Design',
+  ];
+
   toggleFilterButton() {
     this.toggledFilterButton = !this.toggledFilterButton;
+  }
+
+  getOptionSelected(value: string) {
+    console.log('Option selected:', value);
   }
 }

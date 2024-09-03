ad5c87521d2964c830a74dfbdc37e35253d14196
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-03 11:02:23 +0000

    feat: implement the dropdown on select button clicked

--- src/app/shared/components/select-filter/select-filter.component.ts
+++ src/app/shared/components/select-filter/select-filter.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
 
 @Component({
   selector: 'liaison-select-filter',
   standalone: true,
   imports: [],
   templateUrl: './select-filter.component.html',
-  styleUrl: './select-filter.component.scss'
+  styleUrl: './select-filter.component.scss',
 })
 export class SelectFilterComponent {
+  @Input() selection: string = 'Select Filter';
+  @Input() selected: string | undefined;
+  @Input() selectedView: string | undefined;
+  @Input() options: string[] = [];
+  optionSelected = output<string>();
 
+  optionsSelection: boolean = false;
+
+  selectedOption(option: string, index: number) {
+    this.optionsSelection = false;
+
+    this.selected = this.options[index];
+    this.selectedView = this.options[index];
+    this.optionSelected.emit(option);
+  }
+
+  openOption() {
+    this.optionsSelection = !this.optionsSelection;
+  }
 }

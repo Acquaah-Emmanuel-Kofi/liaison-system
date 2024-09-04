f16361c8efb924a28924fa3347e328d96851681a
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-04 13:24:03 +0000

    feat: implement sorting by name in table

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, input, output } from '@angular/core';
+import { Component, Input, input, OnInit, output } from '@angular/core';
 import { TableColumn, TableData } from './table.interface';
 import { PreviewPanelComponent } from './components/preview-panel/preview-panel.component';
 import { CommonModule, NgClass } from '@angular/common';
 import { StatusChipComponent } from '../status-chip/status-chip.component';
-import { getFirstTwoInitials } from '../../helpers/constants.helper';
+import { getFirstTwoInitials, sortByKey } from '../../helpers/constants.helper';
 
 @Component({
   selector: 'liaison-table',
@@ -12,13 +12,24 @@ import { getFirstTwoInitials } from '../../helpers/constants.helper';
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
   @Input() HideCheckbox!: boolean;
   columns = input.required<TableColumn[]>();
   data = input.required<TableData[]>();
 
   selectedRow: TableData | null = null;
   actionClicked = output<TableData>();
+
+  sortOrder: 'asc' | 'desc' = 'asc';
+
+  ngOnInit(): void {
+    this.sortByName();
+  }
+
+  sortByName(): void {
+    sortByKey(this.data(), 'name', this.sortOrder);
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+  }
 
   getNameInitials(name: string) {
     return getFirstTwoInitials(name);

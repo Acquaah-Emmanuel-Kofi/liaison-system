040a9aaf5a207327da9371917d4086966f5583d0
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-05 15:06:48 +0000

    feat: implement a method to encrypt and save token

--- src/app/shared/services/token/token.service.ts
+++ src/app/shared/services/token/token.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+  ACCESS_TOKEN_KEY,
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  saveToLocalStorage,
+} from '../../helpers/constants.helper';
+import { CryptoService } from '../cryptojs/crypto.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TokenService {
+  private _cryptoService = inject(CryptoService);
 
-  constructor() { }
+  constructor() {}
+
+  saveToken(token: string) {
+    const encryptedToken = this._cryptoService.encryptToken(token);
+
+    saveToLocalStorage(ACCESS_TOKEN_KEY, encryptedToken);
+  }
 }

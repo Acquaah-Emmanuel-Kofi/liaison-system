24e94ef13f8c0326d05a871783f00de251a02c28
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-05 15:04:55 +0000

    feat: implement method to generate random encryption key

--- src/app/shared/services/cryptojs/crypto.service.ts
+++ src/app/shared/services/cryptojs/crypto.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CryptoService {
+  private encryptionKey: string;
 
-  constructor() { }
+  constructor() {
+    this.encryptionKey = this.generateKey();
+  }
+
+  /**
+   * Method to generate a random key for encryption on each login
+   */
+  generateKey(): string {
+    return CryptoJS.lib.WordArray.random(32).toString(); // 256-bit key
+  }
 }

424590fa2c62b91b93a53d3db97b6bebf4b5363d
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-05 15:05:33 +0000

    feat: implement a method to encrypt token

--- src/app/shared/services/cryptojs/crypto.service.ts
+++ src/app/shared/services/cryptojs/crypto.service.ts
@@ -17,4 +17,18 @@ export class CryptoService {
   generateKey(): string {
     return CryptoJS.lib.WordArray.random(32).toString(); // 256-bit key
   }
+
+  /**
+   * Encrypts a given token using AES encryption with a randomly generated key.
+   * The encryption key is stored in the `encryptionKey` property of the class.
+   * @param token - The token to be encrypted. It should be a string.
+   * @returns The encrypted token as a string.
+   */
+  encryptToken(token: string): string {
+    const encrypted = CryptoJS.AES.encrypt(
+      token,
+      this.encryptionKey
+    ).toString();
+    return encrypted;
+  }
 }

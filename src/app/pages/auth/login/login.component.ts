6a7e542642a551deaf1e1ab01b67d47e54d4a56c
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-06 11:50:34 +0000

    feat: implement functionality to submit credentials to login user

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,9 +1,16 @@
-import {Component, inject} from '@angular/core';
-import {NgClass, NgIf, NgOptimizedImage} from "@angular/common";
-import {EmailValidator, FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {FloatLabelModule} from "primeng/floatlabel";
-import {ChipsModule} from "primeng/chips";
-import {Button} from "primeng/button";
+import { Component, inject } from '@angular/core';
+import { NgClass, NgIf, NgOptimizedImage } from '@angular/common';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ChipsModule } from 'primeng/chips';
+import { Button } from 'primeng/button';
+import { AuthService } from '../services/auth/auth.service';
+import { ICredentials, ILoginResponse } from '../interfaces/auth.interface';
 
 @Component({
   selector: 'liaison-login',
@@ -16,23 +23,24 @@ import {Button} from "primeng/button";
     NgClass,
     ReactiveFormsModule,
     NgIf,
-    Button
+    Button,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  fb = inject(FormBuilder)
+  fb = inject(FormBuilder);
   public type: string = 'password';
-  public submitted: boolean = false;
+  public isLoading: boolean = false;
   public isText: boolean = false;
 
+  private _authService = inject(AuthService);
+
   loginForm = this.fb.group({
-    email: ['', [ Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
     rememberMe: [false],
   });
-
 
   public hideShowPassword() {
     this.isText = !this.isText;
@@ -41,5 +49,36 @@ export class LoginComponent {
     } else {
       this.type = 'password';
     }
+  }
+
+  handleSubmit() {
+    const credentials = this.loginForm.value;
+
+    if (this.formIsValid()) {
+      this.isLoading = true;
+
+      this._authService.loginUser(credentials as ICredentials).subscribe({
+        next: (response) => {
+          this.handleResponse(response);
+        },
+        error: (error) => {
+          alert(error.message);
+        },
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
+    } else {
+      this.loginForm.markAsTouched();
+      alert('Form is invalid. Please check the fields.');
+    }
+  }
+
+  formIsValid() {
+    return this.loginForm.valid;
+  }
+
+  handleResponse(response: ILoginResponse) {
+    alert(response.message);
   }
 }

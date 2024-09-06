b1d1859f4e7b8ceb2555d5d1ec43578eb1a426ca
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-06 12:13:16 +0000

    feat: structire and implement loader component

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'liaison-loader',
   standalone: true,
   imports: [],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.scss'
+  styleUrl: './loader.component.scss',
 })
 export class LoaderComponent {
-
+  type = input.required<'button' | 'page'>();
 }

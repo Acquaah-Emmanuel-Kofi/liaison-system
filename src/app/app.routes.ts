9ec2be1f013cfe8d60ee15bc03d8950283555c4f
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-07 03:45:44 +0000

    feat: removed unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,14 @@
-import { Routes } from '@angular/router';
-import { AdminDashboardComponent } from './pages/admin/components/admin-dashboard/admin-dashboard.component';
-import { AdminComponent } from './pages/admin/admin.component';
-import { UploadComponent } from './pages/admin/components/upload/upload.component';
-import { StudentsComponent } from './pages/admin/components/students/students.component';
-import { LecturersComponent } from './pages/admin/components/lecturers/lecturers.component';
-import { InternshipsComponent } from './pages/admin/components/internships/internships.component';
+import {Routes} from '@angular/router';
+import {AdminDashboardComponent} from './pages/admin/components/admin-dashboard/admin-dashboard.component';
+import {AdminComponent} from './pages/admin/admin.component';
+import {UploadComponent} from './pages/admin/components/upload/upload.component';
+import {StudentsComponent} from './pages/admin/components/students/students.component';
+import {LecturersComponent} from './pages/admin/components/lecturers/lecturers.component';
+import {InternshipsComponent} from './pages/admin/components/internships/internships.component';
 import {
   UploadStudentComponent
 } from "./pages/admin/components/students/components/uploadStudent/upload-student/upload-student.component";
-import { LoginComponent } from './pages/auth/login/login.component';
-import {
-  StudentsTableComponent
-} from "./pages/admin/components/students/components/students-table/students-table.component";
+import {LoginComponent} from './pages/auth/login/login.component';
 
 export const routes: Routes = [
   {
@@ -39,10 +36,6 @@ export const routes: Routes = [
         path: 'students',
         component: StudentsComponent,
         children:[
-          {
-            path:'table',
-            component:StudentsTableComponent
-          },
           {
             path:'upload',
             component:UploadStudentComponent

3de8b1d0bd167e8f1b05f3186b2ad27d5627526c
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-09 08:32:12 +0000

    feat: implment a method to get all students data from backend

--- src/app/pages/admin/service/table-data/table-data.service.ts
+++ src/app/pages/admin/service/table-data/table-data.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TableDataService {
+  private _http = inject(HttpClient);
 
-  constructor() { }
+  constructor() {}
+
+  public getAllStudents(): Observable<any> {
+    return this._http.get<any>(
+      `${environment.BACKEND_API_BASE_URL}/admin/students`
+    );
+  }
 }

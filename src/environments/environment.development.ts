7b8d1d7a97f3e1e4ec7ffccd176a5eebc03d6434
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-09 10:48:31 +0000

    feat: bring back environment.development files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  DEVELOPMENT: import.meta.env.DEVELOPMENT,
+  BACKEND_API_BASE_URL: import.meta.env.BACKEND_API_BASE_URL,
+  SECRET_KEY: import.meta.env.SECRET_KEY,
+};

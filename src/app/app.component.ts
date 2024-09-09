de232680603d4f4d814f6388ac3c84339fcdd340
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-09 13:45:44 +0000

    refactor: remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StatCardComponent } from './shared/components/stat-card/stat-card.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { INavLinks } from './shared/components/sidebar/sidebar.interface';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { LoginComponent } from './pages/auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    LoginComponent,
-    StatCardComponent,
-    SidebarComponent,
-    NavbarComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'liaison-system';
-  plkisns!: INavLinks[];
-}
+export class AppComponent {}

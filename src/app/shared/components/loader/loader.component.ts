5e9089dd6e706ff5064e9556f755ae07740c4629
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-09 16:35:30 +0000

    feat - updated loader

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,12 +1,17 @@
-import { Component, input } from '@angular/core';
+import {Component, Input, input} from '@angular/core';
+import {NgSwitch, NgSwitchCase} from "@angular/common";
 
 @Component({
   selector: 'liaison-loader',
   standalone: true,
-  imports: [],
+  imports: [
+    NgSwitchCase,
+    NgSwitch
+  ],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss',
 })
 export class LoaderComponent {
-  type = input.required<'button' | 'page'>();
+  @Input() loaderText: string = 'Loading...';
+  @Input() loaderType: 'dots' | 'bars' | 'ring' = 'dots';
 }

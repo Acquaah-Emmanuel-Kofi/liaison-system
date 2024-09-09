cf473b44680280c98da9922918996ad3bb7b5abe
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-09 14:01:30 +0000

    feat: setup tanstack query devtools in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularQueryDevtools],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  devEnvironment: boolean = environment.DEVELOPMENT;
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
-  production: false,
-  BACKEND_API_BASE_URL: 'https://liaison-system-backend.onrender.com',
+  DEVELOPMENT: true,
+  BACKEND_API_BASE_URL:
+    'https://liaison-system-backend.onrender.com/liaison/api/v1',
+  SECRET_KEY:
+    '3271e003b532e652c84e73cb577d4c833a9e125f441055045c0001a9418b632e313d5644354f501ea99d45de98b6fb80b5963a506d788a46b1f3b62868c9542a',
 };

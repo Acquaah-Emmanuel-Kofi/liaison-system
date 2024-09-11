0499c48084390043fffe09fa271054f7d170143b
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-11 12:08:31 +0000

    feat -updated student ui

--- src/app/pages/admin/components/students/students.component.ts
+++ src/app/pages/admin/components/students/students.component.ts
@@ -24,6 +24,10 @@ export class StudentsComponent {
   studentService = inject(StudentTableService);
   selectedRowData: TableData | null = null;
 
+  first = 0
+  pageSize = 0
+  totalData = 0
+
   columns: TableColumn[] = [
     { label: 'Student ID', key: 'student_id' },
     { label: 'Name', key: 'name' },

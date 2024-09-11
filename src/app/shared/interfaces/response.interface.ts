cd5905ce226b038174dd75e5da6381bdac3076c3
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-11 10:47:09 +0000

    refactor: put all global functions and interface to one file

--- src/app/shared/interfaces/response.interface.ts
+++ src/app/shared/interfaces/response.interface.ts
@@ -2,3 +2,22 @@ export interface ICommonResponse {
     status: number;
     message: string;
 }
+
+export interface IGetStudentResponse extends ICommonResponse {
+  data: IStudentData[];
+}
+
+export interface IStudentData {
+  age: string;
+  course: string;
+  department: string;
+  email: string;
+  endDate: string;
+  faculty: string;
+  gender: string;
+  id: string;
+  name: string;
+  phone: string;
+  placeOfInternship: string;
+  startDate: string;
+}

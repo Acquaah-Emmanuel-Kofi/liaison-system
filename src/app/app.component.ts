774b63abb42c375ef98742135a2edfb5d209bf5a
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-12 11:03:36 +0000

    feat - removed universal tooltip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
-import { environment } from '../environments/environment.development';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {AngularQueryDevtools} from '@tanstack/angular-query-devtools-experimental';
+import {environment} from '../environments/environment.development';
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AngularQueryDevtools],
+  imports: [RouterOutlet, AngularQueryDevtools, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent{
   devEnvironment: boolean = environment.DEVELOPMENT;
 }

593da4e885de91c2dac3214faf2a795b25381ed2
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-09-12 10:51:55 +0000

    feat - added tooltip to show new view

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { INavLinks } from '../../shared/components/sidebar/sidebar.interface';
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { ADMIN_NAVLINKS } from '../../shared/helpers/navlinks.helper';
+import {AsyncPipe} from "@angular/common";
+import {SidebarService} from "../../shared/services/sidebar/sidebar.service";
 
 @Component({
   selector: 'liaison-admin',
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent, RouterOutlet],
+  imports: [SidebarComponent, NavbarComponent, RouterOutlet, AsyncPipe],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
 export class AdminComponent {
+  protected sidebarService = inject(SidebarService);
+
+  close = false
   isSidebarVisible: boolean = true;
 
   links: INavLinks[] = ADMIN_NAVLINKS;

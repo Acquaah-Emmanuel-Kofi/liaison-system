9acf2506595a8bcfb22a9c48fced7a8eea435ec8
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-17 11:01:56 +0000

    feat: update lecturer table with data from backend

--- src/app/pages/admin/components/lecturers/lecturers.component.ts
+++ src/app/pages/admin/components/lecturers/lecturers.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { TableComponent } from '../../../../shared/components/table/table.component';
-import { TableColumn, TableData } from '../../../../shared/components/table/table.interface';
+import {
+  TableColumn,
+  TableData,
+} from '../../../../shared/components/table/table.interface';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { lecturersQueryKey } from '../../../../shared/helpers/query-keys.helper';
+import { StudentTableService } from '../../service/students-table/student-table.service';
+import {
+  IGetLecturersResponse,
+  ILecturersData,
+} from '../../../../shared/interfaces/response.interface';
+import { CommonModule } from '@angular/common';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'liaison-lecturers',
   standalone: true,
-  imports: [HeaderComponent, TableComponent],
+  imports: [HeaderComponent, TableComponent, CommonModule],
   templateUrl: './lecturers.component.html',
   styleUrl: './lecturers.component.scss',
 })
@@ -15,36 +27,33 @@ export class LecturersComponent {
     { label: 'Staff ID', key: 'staff_id' },
     { label: 'Name', key: 'name' },
     { label: 'Department', key: 'department' },
-    { label: 'Credit Hours', key: 'credit_hours' },
+    { label: 'Faculty', key: 'faculty' },
     { label: '', key: 'action', isAction: true },
   ];
 
-  data: TableData[] = [
-    {
-      staff_id: '#M234',
-      name: 'Brenda Jones',
-      department: 'Engineering',
-      credit_hours: '32',
+  private _dataServices = inject(StudentTableService);
+
+  lecturersQuery = injectQuery(() => ({
+    queryKey: [...lecturersQueryKey.data()],
+    queryFn: async () => {
+      const response = await lastValueFrom<IGetLecturersResponse>(
+        this._dataServices.getAllLeturers()
+      );
+
+      return this.destructureStudents(response.data.page.content);
     },
-    {
-      staff_id: '#M234',
-      name: 'Brenda Jones',
-      department: 'Engineering',
-      credit_hours: '32',
-    },
-    {
-      staff_id: '#M234',
-      name: 'Brenda Jones',
-      department: 'Engineering',
-      credit_hours: '32',
-    },
-    {
-      staff_id: '#M234',
-      name: 'Brenda Jones',
-      department: 'Engineering',
-      credit_hours: '32',
-    },
-  ];
+  }));
+
+  destructureStudents(data: ILecturersData[] | undefined): TableData[] {
+    if (!data) return [];
+
+    return data.map((lecturer: ILecturersData) => ({
+      staff_id: lecturer.id,
+      name: `${lecturer.firstName} ${lecturer.lastName}`,
+      faculty: lecturer.faculty,
+      department: lecturer.department,
+    }));
+  }
 
   handleRowSelection(row: TableData) {
     console.log('Row selected:', row);

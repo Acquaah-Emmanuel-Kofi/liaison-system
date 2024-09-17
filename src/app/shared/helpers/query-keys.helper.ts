fc714c1e3cf1806d29c7db03021cbff583cdd33b
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-17 10:58:58 +0000

    feat: create lecturer query key

--- src/app/shared/helpers/query-keys.helper.ts
+++ src/app/shared/helpers/query-keys.helper.ts
@@ -2,3 +2,8 @@ export const studentsQueryKey = {
   all: ['students'] as const,
   data: () => [...studentsQueryKey.all, 'all-students'] as const,
 };
+
+export const lecturersQueryKey = {
+  all: ['lecturers'] as const,
+  data: () => [...lecturersQueryKey.all, 'all-lecturers'] as const,
+};

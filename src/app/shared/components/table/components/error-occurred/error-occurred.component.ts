fffca44990e615a0448f38f86f71841d3d1751af
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-19 14:45:51 +0000

    feat: generate error occurred component

---
+++ src/app/shared/components/table/components/error-occurred/error-occurred.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-error-occurred',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-occurred.component.html',
+  styleUrl: './error-occurred.component.scss'
+})
+export class ErrorOccurredComponent {
+
+}

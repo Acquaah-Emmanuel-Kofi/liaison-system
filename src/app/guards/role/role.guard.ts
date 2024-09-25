9efb1563ba9c749af61ea84c2509223c3bf440dd
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-25 12:23:55 +0000

    feat: implement a role guard to protect role routes

--- src/app/guards/role/role.guard.ts
+++ src/app/guards/role/role.guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserStore } from '../../shared/store/user.store';
 
 export const roleGuard: CanActivateFn = (route, state) => {
+  const userStore = inject(UserStore);
+  const router = inject(Router);
+
+  if (!state.url.includes(`${userStore.role().toLowerCase()}`)) {
+    
+    router.navigateByUrl(
+      userStore.role() ? `/${userStore.role().toLowerCase()}` : '/'
+    );
+
+    return false;
+  }
+
   return true;
 };

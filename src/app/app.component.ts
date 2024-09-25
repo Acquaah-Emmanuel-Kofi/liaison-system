7e9a2abf8e86de64c5391935390327c97ebbee03
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-25 11:40:53 +0000

    feat: destroy storage listener in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, isDevMode } from '@angular/core';
+import { Component, isDevMode, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
+import { AuthService } from './pages/auth/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,12 @@ import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimen
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   devEnvironment: boolean = isDevMode();
+
+  constructor(private _authService: AuthService) {}
+
+  ngOnDestroy() {
+    this._authService.destroyStorageListener();
+  }
 }

61c53d6135a5d7a5edb16881ae736738c4f87157
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-25 12:10:30 +0000

    feat: load admin routes with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
-import { AdminDashboardComponent } from './pages/admin/components/admin-dashboard/admin-dashboard.component';
 import { AdminComponent } from './pages/admin/admin.component';
-import { UploadComponent } from './pages/admin/components/upload/upload.component';
-import { StudentsComponent } from './pages/admin/components/students/students.component';
-import { LecturersComponent } from './pages/admin/components/lecturers/lecturers.component';
-import { InternshipsComponent } from './pages/admin/components/internships/internships.component';
-import { UploadStudentComponent } from './pages/admin/components/students/components/uploadStudent/upload-student/upload-student.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { ErrorPageComponent } from './pages/404/error-page/error-page.component';
-import {ZonesComponent} from "./pages/admin/components/zones/zones.component";
+import { authGuard } from './guards/auth/auth.guard';
 
 export const routes: Routes = [
   {
@@ -23,43 +17,11 @@ export const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
-      },
-      {
-        path: 'dashboard',
-        component: AdminDashboardComponent,
-      },
-      {
-        path: 'upload',
-        component: UploadComponent,
-      },
-      {
-        path: 'students',
-        component: StudentsComponent,
-        children: [
-          {
-            path: 'upload',
-            component: UploadStudentComponent,
-          },
-        ],
-      },
-      {
-        path: 'lecturers',
-        component: LecturersComponent,
-      },
-      {
-        path: 'internships',
-        component: InternshipsComponent,
-      },
-      {
-        path: "zones",
-        component: ZonesComponent
-      }
-    ],
+    loadChildren: () =>
+      import('../app/pages/admin/admin.routes').then(
+        (routes) => routes.adminRoutes
+      ),
+    canActivate: [authGuard],
   },
   {
     path: '**',

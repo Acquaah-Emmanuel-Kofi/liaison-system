37e14402cc3bd4f3f00d46cb4649e378fb8d2ae5
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-25 11:55:14 +0000

    feat: implement a functionality to protect routes

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../pages/auth/services/auth/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isAuthenticated()) {
+    router.navigateByUrl('/login');
+    return false;
+  }
+
   return true;
 };

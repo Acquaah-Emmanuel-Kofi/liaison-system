eeb06b0580efc8aa89e91f83b94037350b3c3d59
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-25 12:24:28 +0000

    feat: protect admin route with role guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AdminComponent } from './pages/admin/admin.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { ErrorPageComponent } from './pages/404/error-page/error-page.component';
 import { authGuard } from './guards/auth/auth.guard';
+import { roleGuard } from './guards/role/role.guard';
 
 export const routes: Routes = [
   {
@@ -21,7 +22,7 @@ export const routes: Routes = [
       import('../app/pages/admin/admin.routes').then(
         (routes) => routes.adminRoutes
       ),
-    canActivate: [authGuard],
+    canActivate: [authGuard, roleGuard],
   },
   {
     path: '**',

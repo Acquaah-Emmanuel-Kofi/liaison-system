5d8b90d21bbd3a5e351c157f0dd97cd08675d3ce
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-26 17:54:22 +0000

    refactor: remove unwanted lines of code

--- src/app/pages/admin/components/internships/components/header/header.component.ts
+++ src/app/pages/admin/components/internships/components/header/header.component.ts
@@ -11,26 +11,6 @@ import { SearchbarComponent } from '../../../../../../shared/components/searchba
 })
 export class HeaderComponent {
   searchValue = output<string>();
-  toggledFilterButton: boolean = false;
-
-  filterOptions: string[] = [
-    'Computer Science',
-    'Engineering',
-    'Fashion Design',
-    'Textiles Design',
-    'Ceramic Design',
-    'Painting Design',
-    'Sculpture Design',
-    'Graphic Design',
-  ];
-
-  toggleFilterButton() {
-    this.toggledFilterButton = !this.toggledFilterButton;
-  }
-
-  getOptionSelected(value: string) {
-    console.log('Option selected:', value);
-  }
 
   handleSearchTerm(value: string) {
     this.searchValue.emit(value);

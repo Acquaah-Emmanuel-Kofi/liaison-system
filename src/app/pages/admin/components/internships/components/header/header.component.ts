47b13efc5b861b537254b567f02426f7135254f9
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-26 18:00:15 +0000

    feat: add filters to internship header component

--- src/app/pages/admin/components/internships/components/header/header.component.ts
+++ src/app/pages/admin/components/internships/components/header/header.component.ts
@@ -1,16 +1,68 @@
-import { Component, output } from '@angular/core';
+import { Component, OnInit, output } from '@angular/core';
 import { SelectFilterComponent } from '../../../../../../shared/components/select-filter/select-filter.component';
 import { SearchbarComponent } from '../../../../../../shared/components/searchbar/searchbar.component';
+import { facultiesAndDepartments } from '../../../../../../shared/helpers/constants.helper';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'liaison-header',
   standalone: true,
-  imports: [SearchbarComponent, SelectFilterComponent],
+  imports: [
+    SearchbarComponent,
+    SelectFilterComponent,
+    DropdownModule,
+    FormsModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   searchValue = output<string>();
+  filterValues = output<{ faculty: string; department: string }>();
+  refetch = output<void>();
+
+  toggledFilters: boolean = false;
+
+  facultyFilterOptions: { name: string; value: string }[] = [];
+  departmentsFilterOptions: { name: string; value: string }[] = [];
+
+  selectedFaculty: string | null = null;
+  selectedDepartment: string | null = null;
+
+  facultiesAndDepartments: Record<string, { name: string; value: string }[]> =
+    {};
+
+  constructor() {
+    this.facultiesAndDepartments = facultiesAndDepartments;
+  }
+
+  ngOnInit() {
+    this.facultyFilterOptions = Object.keys(this.facultiesAndDepartments).map(
+      (faculty) => ({
+        name: faculty,
+        value: faculty,
+      })
+    );
+  }
+
+  toggleFilter() {
+    this.toggledFilters = !this.toggledFilters;
+  }
+
+  onFacultyChange(faculty: string) {
+    this.departmentsFilterOptions = this.facultiesAndDepartments[faculty] || [];
+    this.selectedDepartment = null;
+  }
+
+  emitFilterValue() {
+    const selectedData = {
+      faculty: this.selectedFaculty ?? '',
+      department: this.selectedDepartment ?? '',
+    };
+
+    this.filterValues.emit(selectedData);
+  }
 
   handleSearchTerm(value: string) {
     this.searchValue.emit(value);

b069919a60b504750392058ed944d95cc11c9809
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-11-12 15:10:20 +0000

    feat - adjusting endpoint for submitting excel sheet

--- src/app/pages/admin/service/student-upload/data.service.ts
+++ src/app/pages/admin/service/student-upload/data.service.ts
@@ -1,25 +1,29 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import { ICommonResponse } from '../../../../shared/interfaces/response.interface';
 import { environment } from '../../../../../environments/environment.development';
+import {UserStore} from "../../../../shared/store/user.store";
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   http = inject(HttpClient);
+  private readonly  userStore = inject(UserStore)
   students: any[] = [];
-  headers: string[] = [];
+  headers: string[] = []
 
-  sendFileToBackend(file: File): Promise<ICommonResponse> {
+
+  sendFileToBackend(file: File,internship: boolean): Promise<ICommonResponse> {
+    const params = new HttpParams().set('internship',internship)
     const formData = new FormData();
     formData.append('file', file);
 
     return lastValueFrom(
       this.http.post<ICommonResponse>(
-        `${environment.BACKEND_API_BASE_URL}/admin/students`,
-        formData
+        `${environment.BACKEND_API_BASE_URL}/admin/${this.userStore.id()}/students`,
+        formData,{params}
       )
     );
   }

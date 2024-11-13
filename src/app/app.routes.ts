ec49c040e135941138a8c898e52635b964955064
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-13 13:32:24 +0000

    feat: lazy load lecturer routes in app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ErrorPageComponent } from './pages/404/error-page/error-page.component'
 import { authGuard } from './guards/auth/auth.guard';
 import { roleGuard } from './guards/role/role.guard';
 import { guestGuard } from './guards/guest/guest.guard';
+import { LecturerComponent } from './pages/lecturer/lecturer.component';
 
 export const routes: Routes = [
   {
@@ -25,6 +26,14 @@ export const routes: Routes = [
         (routes) => routes.adminRoutes
       ),
     canActivate: [authGuard, roleGuard],
+  },
+  {
+    path: 'lecturer',
+    component: LecturerComponent,
+    loadChildren: () =>
+      import('../app/pages/lecturer/lecturer.routes').then(
+        (routes) => routes.lecturerRoutes
+      ),
   },
   {
     path: '**',

102f3542932c31aa5b58be8c5c78c4e00018ca8e
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-14 09:00:45 +0000

    feat: implement a functionality to load map

--- src/app/pages/lecturer/services/location/location.service.ts
+++ src/app/pages/lecturer/services/location/location.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ILecturerLocation } from '../../interfaces/location.interface';
+import { environment } from '../../../../../environments/environment.development';
 
+declare var google: any;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class LocationService {
+export class LocationService implements OnInit {
+  private isLoaded = false;
 
-  constructor() { }
+  private loadMap(): Promise<void> {
+    if (this.isLoaded) return Promise.resolve();
+
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.GOOGLE_MAP_API_KEY}&libraries=places`;
+      script.async = true;
+      script.defer = true;
+      script.onload = () => {
+        this.isLoaded = true;
+        resolve();
+      };
+      script.onerror = (error) => reject(error);
+
+      document.head.appendChild(script);
+    });
+  }
+
+  private directionsService!: google.maps.DirectionsService;
+  private directionsRenderer!: google.maps.DirectionsRenderer;
+
+  ngOnInit(): void {
+    this.loadMap()
+      .then(() => {
+        this.directionsService = new google.maps.DirectionsService();
+        this.directionsRenderer = new google.maps.DirectionsRenderer();
+      })
+      .catch((error) => {
+        console.error('Google Maps API failed to load', error);
+      });
+  }
+
+  
 }

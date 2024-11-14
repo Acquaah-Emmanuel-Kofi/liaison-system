2b959b2678ee30059cafb814f34cae941e5febc7
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-14 21:15:56 +0000

    feat: build variables store

---
+++ src/app/shared/store/global-variables.store.ts
@@ -0,0 +1,34 @@
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { withDevtools } from '@angular-architects/ngrx-toolkit';
+import {
+  setEndOfAcademicYear,
+  setInternshipType,
+  setStartOfAcademicYear,
+} from './global-variables.actions';
+import { IGlobalVariables } from '../interfaces/global-variables.interface';
+
+const currentYear = new Date().getFullYear();
+const lastyear = currentYear - 1;
+
+const initialInternshipTypeState: IGlobalVariables = {
+  type: false,
+  startYear: lastyear,
+  endYear: currentYear,
+};
+
+export const GlobalVariablesStore = signalStore(
+  { providedIn: 'root' },
+  withDevtools('global'),
+  withState(initialInternshipTypeState),
+  withMethods((store) => ({
+    setInternshipType: (payload: boolean) => {
+      patchState(store, setInternshipType(payload));
+    },
+    setStartOfAcademicYear: (payload: number) => {
+      patchState(store, setStartOfAcademicYear(payload));
+    },
+    setEndOfAcademicYear: (payload: number) => {
+      patchState(store, setEndOfAcademicYear(payload));
+    },
+  }))
+);

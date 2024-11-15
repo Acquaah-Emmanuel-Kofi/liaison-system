0e858dcd06b28220da086f67456bf8b4873d0808
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 11:09:54 +0000

    feat: render lecturer student header component on students view page

--- src/app/pages/lecturer/components/students/students.component.ts
+++ src/app/pages/lecturer/components/students/students.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LecturerStudentsHeaderComponent } from './components/lecturer-students-header/lecturer-students-header.component';
 
 @Component({
   selector: 'liaison-students',
   standalone: true,
-  imports: [],
+  imports: [LecturerStudentsHeaderComponent],
   templateUrl: './students.component.html',
   styleUrl: './students.component.scss'
 })

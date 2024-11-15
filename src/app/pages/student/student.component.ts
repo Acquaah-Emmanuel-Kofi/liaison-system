9483c42e2cfb66b64f23c4c9bece0ab72356a703
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 22:08:26 +0000

    feat: generate student role component

---
+++ src/app/pages/student/student.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.scss'
+})
+export class StudentComponent {
+
+}

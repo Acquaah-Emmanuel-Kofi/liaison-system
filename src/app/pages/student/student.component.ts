42f12063c43759cc83673a70e616a4e54c898934
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 22:42:01 +0000

    feat: build student layout

--- src/app/pages/student/student.component.ts
+++ src/app/pages/student/student.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SidebarService } from '../../shared/services/sidebar/sidebar.service';
+import { INavLinks } from '../../shared/components/sidebar/sidebar.interface';
+import { STUDENT_NAVLINKS } from '../../shared/helpers/navlinks.helper';
+import { AsyncPipe } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'liaison-student',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, NavbarComponent, RouterOutlet, AsyncPipe],
   templateUrl: './student.component.html',
-  styleUrl: './student.component.scss'
+  styleUrl: './student.component.scss',
 })
 export class StudentComponent {
+  protected sidebarService = inject(SidebarService);
 
+  close = true;
+  isSidebarVisible: boolean = true;
+
+  links: INavLinks[] = STUDENT_NAVLINKS;
+
+  public toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible;
+  }
 }

0f166cc205e697cf25570b1ce5288ddc9e693aeb
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 10:51:39 +0000

    feat: generate lecturer students header component

---
+++ src/app/pages/lecturer/components/students/components/lecturer-students-header/lecturer-students-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-lecturer-students-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './lecturer-students-header.component.html',
+  styleUrl: './lecturer-students-header.component.scss'
+})
+export class LecturerStudentsHeaderComponent {
+
+}

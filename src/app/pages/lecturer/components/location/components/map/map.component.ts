7be34822c7cd1c41eea10f3f5da2cbc396582026
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-19 12:56:34 +0000

    feat: generate map component

---
+++ src/app/pages/lecturer/components/location/components/map/map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent {
+
+}

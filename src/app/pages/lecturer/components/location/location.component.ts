3ac3793e62c3817c95ea82e12e8b5753c638cf32
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-19 13:20:14 +0000

    feat: restructure lecture page with panel preview

--- src/app/pages/lecturer/components/location/location.component.ts
+++ src/app/pages/lecturer/components/location/location.component.ts
@@ -1,59 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { LocationService } from '../../services/location/location.service';
-import {
-  ILecturerLocation,
-  IStudentLocation,
-} from '../../interfaces/location.interface';
-
+import { Component } from '@angular/core';
+import { MapComponent } from './components/map/map.component';
 @Component({
   selector: 'liaison-location',
   standalone: true,
-  imports: [GoogleMapsModule],
+  imports: [MapComponent],
   templateUrl: './location.component.html',
   styleUrl: './location.component.scss',
 })
 export class LocationComponent {
-  userLocation = signal<ILecturerLocation>({
-    latitude: 7.9465,
-    longitude: -1.0232,
-  });
 
-  center: google.maps.LatLngLiteral = { lat: 7.9465, lng: 1.0232 };
-  map: google.maps.Map | undefined;
-
-  studentLocations: IStudentLocation[] = [
-    { lat: 7.9465, lng: -1.0232, label: 'Student 1' },
-    { lat: 8.9465, lng: -1.0232, label: 'Student 2' },
-    { lat: 6.9465, lng: -2.0232, label: 'Student 3' },
-    { lat: 7.1465, lng: -3.0232, label: 'Student 4' },
-  ];
-
-  private locationService = inject(LocationService);
-
-  ngOnInit(): void {
-    this.locationService.getUserLocation().subscribe((location) => {
-      this.center = { lat: location.latitude, lng: location.longitude };
-      this.userLocation.set(location);
-    });
-  }
-
-  onMapReady(mapInstance: google.maps.Map): void {
-    this.map = mapInstance;
-  }
-
-  calculateRouteToStudent(studentLocation: google.maps.LatLngLiteral): void {
-    const lecturerLocation: google.maps.LatLngLiteral = {
-      lat: this.userLocation().latitude,
-      lng: this.userLocation().longitude,
-    };
-
-    if (this.map) {
-      this.locationService.calculateRoute(
-        lecturerLocation,
-        studentLocation,
-        this.map
-      );
-    }
-  }
 }

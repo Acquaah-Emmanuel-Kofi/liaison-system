2239dd7089b7f6642c19e6d64e23504445535815
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-19 12:42:04 +0000

    feat: center map wiht user location

--- src/app/pages/lecturer/components/location/location.component.ts
+++ src/app/pages/lecturer/components/location/location.component.ts
@@ -19,21 +19,22 @@ export class LocationComponent {
     longitude: -1.0232,
   });
 
-  center: google.maps.LatLngLiteral = { lat: -34.397, lng: 150.644 };
-  zoom = 10;
+  center: google.maps.LatLngLiteral = { lat: 7.9465, lng: 1.0232 };
+  map: google.maps.Map | undefined;
+
   studentLocations: IStudentLocation[] = [
     { lat: 7.9465, lng: -1.0232, label: 'Student 1' },
     { lat: 8.9465, lng: -1.0232, label: 'Student 2' },
     { lat: 6.9465, lng: -2.0232, label: 'Student 3' },
     { lat: 7.1465, lng: -3.0232, label: 'Student 4' },
   ];
-  map: google.maps.Map | undefined;
 
   private locationService = inject(LocationService);
 
   ngOnInit(): void {
-    this.locationService.getUserLocation().subscribe((response) => {
-      this.userLocation.set(response);
+    this.locationService.getUserLocation().subscribe((location) => {
+      this.center = { lat: location.latitude, lng: location.longitude };
+      this.userLocation.set(location);
     });
   }
 

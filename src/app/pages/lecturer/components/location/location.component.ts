b9d364805dd82c5af926dfd15a345818f76aecd7
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-19 14:15:47 +0000

    feat: get student location from map to toggle the panel

--- src/app/pages/lecturer/components/location/location.component.ts
+++ src/app/pages/lecturer/components/location/location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MapComponent } from './components/map/map.component';
+import { IStudentLocation } from '../../interfaces/location.interface';
 @Component({
   selector: 'liaison-location',
   standalone: true,
@@ -8,5 +9,13 @@ import { MapComponent } from './components/map/map.component';
   styleUrl: './location.component.scss',
 })
 export class LocationComponent {
+  studentLocation = signal<IStudentLocation | null>(null);
 
+  getStudentLocationToTogglePanel(location: IStudentLocation) {
+    this.studentLocation.set(location);
+  }
+
+  closePanel() {
+    this.studentLocation.set(null)
+  }
 }

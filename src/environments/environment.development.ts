3cfa26292a85fb3f97867f9448b474139694d283
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-27 09:41:08 +0000

    refactor: update API's in env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   BACKEND_API_BASE_URL:
-    'https://liaison-system-backend.onrender.com/liaison/api/v1',
-  GOOGLE_MAP_API_KEY: 'AIzaSyBA_aVNa455ya9X5ruDHWIEvT0WivJHl3Q',
+    'https://liaison-system-backend-m7pq.onrender.com/liaison/api/v1',
+  GOOGLE_MAP_API_KEY: 'AIzaSyB2O9lwBLewaT6IdcC_MKHarVUBkoYXIqE',
 };

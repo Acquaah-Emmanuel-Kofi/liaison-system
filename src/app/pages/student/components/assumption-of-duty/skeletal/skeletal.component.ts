8aaeac70dd09f3d33b10ee856e0b0e8d467b3feb
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-12-09 20:42:19 +0000

    feat - implements a skeletal component for assumption of duty

---
+++ src/app/pages/student/components/assumption-of-duty/skeletal/skeletal.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'liaison-skeletal',
+  standalone: true,
+  imports: [],
+  templateUrl: './skeletal.component.html',
+  styleUrl: './skeletal.component.scss'
+})
+export class SkeletalComponent {
+
+}

2defae37ecf2fe83c27c8dd7533091aa9cab3e98
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-12-10 20:27:14 +0000

    feat: load student location data

--- src/app/pages/admin/components/students/components/student-location/student-location.component.ts
+++ src/app/pages/admin/components/students/components/student-location/student-location.component.ts
@@ -1,5 +1,10 @@
-import { Component, signal, viewChild } from '@angular/core';
+import { Component, inject, signal, viewChild } from '@angular/core';
 import { MapComponent } from '../../../../../../shared/components/map/map.component';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { studentsLocationQueryKey } from '../../../../../../shared/helpers/query-keys.helper';
+import { DashboardService } from '../../../../service/dashboard/dashboard.service';
+import { GlobalVariablesStore } from '../../../../../../shared/store/global-variables.store';
+import { IStudentCompanyMapping } from '../../../../../lecturer/interfaces/location.interface';
 
 @Component({
   selector: 'liaison-student-location',
@@ -11,6 +16,11 @@ import { MapComponent } from '../../../../../../shared/components/map/map.compon
 export class StudentLocationComponent {
   studentLocation = signal<any | null>(null);
   mapComponent = viewChild(MapComponent);
+
+  studentData = signal<IStudentCompanyMapping[]>([]);
+
+  private dashboardService = inject(DashboardService);
+  private globalStore = inject(GlobalVariablesStore);
 
   getStudentLocationToTogglePanel(location: any) {
     this.studentLocation.set(location);
@@ -31,4 +41,21 @@ export class StudentLocationComponent {
       alert('Something went wrong.');
     }
   }
+
+  studentsLocationQuery = injectQuery(() => ({
+    queryKey: [
+      ...studentsLocationQueryKey.data(
+        this.globalStore.type(),
+        this.globalStore.startYear(),
+        this.globalStore.endYear()
+      ),
+    ],
+    queryFn: async () => {
+      const response = await this.dashboardService.getStudentsLocation();
+
+      this.studentData.set(response.data);
+
+      return response.data;
+    },
+  }));
 }

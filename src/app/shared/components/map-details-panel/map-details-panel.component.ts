b46035ebba06e58423175dfc735460125faa223b
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-12-10 21:53:51 +0000

    feat: build reusable map details panel component

---
+++ src/app/shared/components/map-details-panel/map-details-panel.component.ts
@@ -0,0 +1,14 @@
+import { Component, input, output } from '@angular/core';
+import { IStudentCompanyMapping } from '../../../pages/lecturer/interfaces/location.interface';
+
+@Component({
+  selector: 'liaison-map-details-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-details-panel.component.html',
+  styleUrl: './map-details-panel.component.scss',
+})
+export class MapDetailsPanelComponent {
+  studentData = input.required<IStudentCompanyMapping | null>();
+  closePanel = output<void>();
+}

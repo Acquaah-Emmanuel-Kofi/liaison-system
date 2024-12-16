3e64c88be6d0a41b71c56fd9e439d62a0a455ef9
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-12-16 00:22:35 +0000

    feat: implement a method to update assumption of duty

--- src/app/pages/student/services/assumption/assumption.service.ts
+++ src/app/pages/student/services/assumption/assumption.service.ts
@@ -3,31 +3,44 @@ import {UserStore} from "../../../../shared/store/user.store";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {catchError, Observable, tap} from "rxjs";
 import {environment} from "../../../../../environments/environment.development";
+import { GlobalVariablesStore } from '../../../../shared/store/global-variables.store';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AssumptionService {
-  private readonly  userStore = inject(UserStore)
+  private readonly userStore = inject(UserStore);
+  private readonly globalStore = inject(GlobalVariablesStore);
 
   _http = inject(HttpClient);
-  constructor() { }
 
-  submitAssumptionForm(formData:any, startYear:number, endYear:number, internship:boolean): Observable<any>{
+  submitAssumptionForm(formData: any): Observable<any> {
     const params = new HttpParams()
-      .set('startOfAcademicYear', startYear.toString())
-      .set('endOfAcademicYear', endYear.toString())
-      .set('internship', internship);
+      .set('startOfAcademicYear', this.globalStore.startYear().toString())
+      .set('endOfAcademicYear', this.globalStore.endYear().toString())
+      .set('internship', this.globalStore.type());
 
-    const url = `${environment.BACKEND_API_BASE_URL}/assumption-of-duty/${this.userStore.id()}`;
+    const url = `${
+      environment.BACKEND_API_BASE_URL
+    }/assumption-of-duty/${this.userStore.id()}`;
 
     return this._http.post(url, formData, { params }).pipe(
-      tap((response:any) => console.log('Response:', response)),
-      catchError(error => {
+      catchError((error) => {
         throw error;
       })
     );
   }
 
 
+  updateAssuptionOfDuty(formData: any, dutyId: string): Observable<any> {
+    const url = `${
+      environment.BACKEND_API_BASE_URL
+    }/assumption-of-duty/${this.userStore.id()}/update/${dutyId}`;
+
+    return this._http.patch(url, formData).pipe(
+      catchError((error) => {
+        throw error;
+      })
+    );
+  }
 }
--- src/app/pages/student/services/dashboard/dashboard.service.ts
+++ src/app/pages/student/services/dashboard/dashboard.service.ts
@@ -1,21 +1,23 @@
-import {inject, Injectable} from '@angular/core';
-import {UserStore} from "../../../../shared/store/user.store";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {catchError, lastValueFrom, Observable, tap} from "rxjs";
-import {environment} from "../../../../../environments/environment.development";
+import { inject, Injectable } from '@angular/core';
+import { UserStore } from '../../../../shared/store/user.store';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DashboardService {
-  private readonly  userStore = inject(UserStore)
+  private readonly userStore = inject(UserStore);
   private studentData: any;
 
   _http = inject(HttpClient);
 
-  getDashboardInfo(){
-    const url = `${environment.BACKEND_API_BASE_URL}/student/dashboard/${this.userStore.id()}`;
-    return lastValueFrom( this._http.get<any>(url));
+  getDashboardInfo() {
+    const url = `${
+      environment.BACKEND_API_BASE_URL
+    }/student/dashboard/${this.userStore.id()}`;
+    return lastValueFrom(this._http.get<any>(url));
   }
 
   setDashboardData(data: any) {

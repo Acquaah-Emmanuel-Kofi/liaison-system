7ac23f52805c24663d46849f07707cc4928a24b4
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-12-16 12:02:26 +0000

    feat: implement a method to get assumption of duty logs

--- src/app/pages/admin/service/dashboard/dashboard.service.ts
+++ src/app/pages/admin/service/dashboard/dashboard.service.ts
@@ -3,7 +3,10 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../../environments/environment.development';
 import { lastValueFrom } from 'rxjs';
 import { UserStore } from '../../../../shared/store/user.store';
-import { IStatAnalyticsResponse, IStudentCompanyMappingResponse } from '../../../../shared/interfaces/response.interface';
+import {
+  IStatAnalyticsResponse,
+  IStudentCompanyMappingResponse,
+} from '../../../../shared/interfaces/response.interface';
 import { GlobalVariablesStore } from '../../../../shared/store/global-variables.store';
 
 @Injectable({
@@ -17,33 +20,48 @@ export class DashboardService {
 
   constructor() {}
 
-  getStatAnalytics(): Promise<IStatAnalyticsResponse> {
-    const params = new HttpParams()
+  private httpParams(): HttpParams {
+    return new HttpParams()
       .set('startOfAcademicYear', this.globalStore.startYear())
       .set('endOfAcademicYear', this.globalStore.endYear())
-      .set('internship', this.globalStore.type().toString());
+      .set('internship', this.globalStore.type().toString())
+      .set('page', 0)
+      .set('size', 1);
+  }
 
+  getStatAnalytics(): Promise<IStatAnalyticsResponse> {
     const endpoint = `${
       environment.BACKEND_API_BASE_URL
     }/admin/dashboard/${this.userStore.id()}`;
 
     return lastValueFrom(
-      this._http.get<IStatAnalyticsResponse>(endpoint, { params })
+      this._http.get<IStatAnalyticsResponse>(endpoint, {
+        params: this.httpParams(),
+      })
     );
   }
 
   getStudentsLocation(): Promise<IStudentCompanyMappingResponse> {
-    const params = new HttpParams()
-      .set('startOfAcademicYear', this.globalStore.startYear())
-      .set('endOfAcademicYear', this.globalStore.endYear())
-      .set('internship', this.globalStore.type().toString());
-
     const endpoint = `${
       environment.BACKEND_API_BASE_URL
     }/admin/${this.userStore.id()}/students/location`;
 
     return lastValueFrom(
-      this._http.get<IStudentCompanyMappingResponse>(endpoint, { params })
+      this._http.get<IStudentCompanyMappingResponse>(endpoint, {
+        params: this.httpParams(),
+      })
+    );
+  }
+
+  getAssumptionOfDutyLogs(): Promise<any> {
+    const endpoint = `${
+      environment.BACKEND_API_BASE_URL
+    }/admin/${this.userStore.id()}/assumption-of-duties/updated`;
+
+    return lastValueFrom(
+      this._http.get<any>(endpoint, {
+        params: this.httpParams(),
+      })
     );
   }
 }

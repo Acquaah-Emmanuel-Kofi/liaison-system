c29dc0ea6b75e5af788ae669709a5cbb36fe30f2
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-01-03 23:14:05 +0000

    feat: fetch duty details data from backend with tanstack query

--- src/app/pages/admin/components/duty-details/duty-details.component.ts
+++ src/app/pages/admin/components/duty-details/duty-details.component.ts
@@ -1,13 +1,36 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { DashboardService } from '../../service/dashboard/dashboard.service';
+import { studentAssumptionOfDutyLogsQueryKey } from '../../../../shared/helpers/query-keys.helper';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'liaison-duty-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, DatePipe],
   templateUrl: './duty-details.component.html',
-  styleUrl: './duty-details.component.scss'
+  styleUrl: './duty-details.component.scss',
 })
 export class DutyDetailsComponent {
   id = input.required<string>();
+
+  private _dashboardService = inject(DashboardService);
+
+  dutyDetailsQuery = injectQuery(() => ({
+    queryKey: [...studentAssumptionOfDutyLogsQueryKey.details()],
+    queryFn: async () => {
+      const response =
+        await this._dashboardService.getUpdatedAssumptionOfDutyDetails(
+          this.id()
+        );
+
+      const data = {
+        oldDuty: response.data.oldAssumptionOfDuties[0],
+        updatedDuty: response.data.updatedAssumptionOfDuty,
+      };
+
+      return data;
+    },
+  }));
 }

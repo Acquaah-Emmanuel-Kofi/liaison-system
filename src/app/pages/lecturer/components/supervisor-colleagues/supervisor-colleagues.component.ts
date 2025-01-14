2e78f5bb1327fc51bc2b0efcd11dc0515252a133
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-01-14 17:17:17 +0000

    feat: generate suppervisor colleagues component

---
+++ src/app/pages/lecturer/components/supervisor-colleagues/supervisor-colleagues.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-supervisor-colleagues',
+  standalone: true,
+  imports: [],
+  templateUrl: './supervisor-colleagues.component.html',
+  styleUrl: './supervisor-colleagues.component.scss'
+})
+export class SupervisorColleaguesComponent {
+
+}

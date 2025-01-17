28cf9d2ff944780f21994c705a4da20926a45999
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-01-17 09:00:19 +0000

    feat: generate industries component

---
+++ src/app/pages/lecturer/components/industries/industries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liaison-industries',
+  standalone: true,
+  imports: [],
+  templateUrl: './industries.component.html',
+  styleUrl: './industries.component.scss'
+})
+export class IndustriesComponent {
+
+}
